fcc54acb421da9d087c4c4877ed00cbdae31dd97
walid-el-fissaoui <walid.elfissaoui@gmail.com>
2023-01-09 18:41:11 +0100

    #1 setup the project and deal with component's template and style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: `<h1>Hello Angular</h1>
+  // <p>This is inline html</p>`,
   styleUrls: ['./app.component.css']
+  // styles: [`h1 {color: red;}
+  //   p {background-color: yellow;}
+  // `]
 })
 export class AppComponent {
   title = 'AngularFirstProject';
